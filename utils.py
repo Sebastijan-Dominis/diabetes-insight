b420c9eb4b4d87c01dd1fa6bc7de17d0540091c1
Sebastijan-Dominis <sebastijan.dominis99@gmail.com>
2025-11-09 18:57:15 +0100

    Created a better utility function for evaluating metrics.

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import roc_curve, auc
 
 def load_and_prepare_data(df):
     X = df.drop(columns=["diagnosed_diabetes"])
@@ -22,7 +24,10 @@ def load_and_prepare_data(df):
 
     return X_train_std, X_test_std, y_train, y_test
 
-def print_confusion_matrix(actual, predicted):
+def evaluate_metrics(actual, predicted, title=None):
+    if title:
+        print(title)
+
     matrix = confusion_matrix(actual, predicted)
     ax = sns.heatmap(
         matrix,
@@ -37,7 +42,10 @@ def print_confusion_matrix(actual, predicted):
     ax.set_title("Diabetes Diagnosis - Confusion Matrix")
     plt.show();
 
-from sklearn.metrics import roc_curve, auc
+    print(f"Accuracy: {accuracy_score(actual, predicted)}")
+    print(f"Precision: {precision_score(actual, predicted)}")
+    print(f"Recall: {recall_score(actual, predicted)}")
+    print(f"F1 Score: {f1_score(actual, predicted)}")
     
 def plot_roc_curve(y_true, y_probs, model_name='Model'):
     fpr, tpr, thresholds = roc_curve(y_true, y_probs)
