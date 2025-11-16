27fe843cb83942f245dc1c9735d882b0cb857f8f
Sebastijan-Dominis <sebastijan.dominis99@gmail.com>
2025-11-16 16:42:13 +0100

    Prepared the backend without LLM.

--- backend/routers/reports.py
+++ backend/routers/reports.py
@@ -1,8 +1,9 @@
 import joblib
+import shap
+import pandas as pd
 from dotenv import load_dotenv
 from fastapi import APIRouter
 from starlette import status
-from sklearn.preprocessing import StandardScaler
 
 load_dotenv()
 
@@ -11,16 +12,14 @@ router = APIRouter(
     tags=["reports"],
 )
 
-thresh1 = 0.28
-scaler1 = joblib.load("backend/models/has_or_not_scaler.pkl")
+thresh1 = 0.3
+thresh2 = 0.77
 
-scaler2 = joblib.load("backend/models/risk_score_scaler.pkl")
+chosen_columns_1 = ['glucose_fasting', 'hba1c']
 
-def prepare_for_diagnosis(hba1c: float, glucose_fasting: int):
-    input_data = [[hba1c, glucose_fasting]]
-    return scaler1.transform(input_data)
+chosen_columns_2 = ["family_history_diabetes", "age_30-39", "age_40-49", "age_50-59", "age_60-69", "age_70-79", "age_80+", "physical_activity_minutes_per_week_Moderate", "physical_activity_minutes_per_week_Active", "physical_activity_minutes_per_week_Very Active", "bmi_Normal", "bmi_Overweight", "bmi_Obese_I", "bmi_Obese_II", "waist_to_hip_ratio", "alcohol_consumption_per_week_Light", "alcohol_consumption_per_week_Moderate", "alcohol_consumption_per_week_Heavy"]
 
-def prepare_for_diagnosis_2(family_history, bmi, age, waist_to_hip_ratio, physical_activity):
+def prepare_for_diagnosis(family_history, bmi, age, waist_to_hip_ratio, physical_activity, alcohol_consumption_per_week):
     age_30_39 = 1 if 30 <= age <= 39 else 0
     age_40_49 = 1 if 40 <= age <= 49 else 0
     age_50_59 = 1 if 50 <= age <= 59 else 0
@@ -37,25 +36,42 @@ def prepare_for_diagnosis_2(family_history, bmi, age, waist_to_hip_ratio, physic
     bmi_Obese_I = 1 if 30 <= bmi < 35 else 0
     bmi_Obese_II = 1 if 35 <= bmi < 40 else 0
 
-    input_data = [[family_history, age_60_69, age_50_59, physical_activity_Active, age_70_79, age_40_49, waist_to_hip_ratio, age_30_39, physical_activity_Very_Active, physical_activity_Moderate, age_80_plus, bmi_Normal, bmi_Overweight, bmi_Obese_I, bmi_Obese_II]]
+    alcohol_consumption_per_week_Light = 1 if 0 < alcohol_consumption_per_week <= 3 else 0
+    alcohol_consumption_per_week_Moderate = 1 if 3 < alcohol_consumption_per_week <= 7 else 0
+    alcohol_consumption_per_week_Heavy = 1 if (7 < alcohol_consumption_per_week) and (alcohol_consumption_per_week != 0) else 0
 
-    return scaler2.transform(input_data)
+    input_data = [[family_history, age_30_39, age_40_49, age_50_59, age_60_69, age_70_79, age_80_plus, physical_activity_Moderate, physical_activity_Active, physical_activity_Very_Active, bmi_Normal, bmi_Overweight, bmi_Obese_I, bmi_Obese_II, waist_to_hip_ratio, alcohol_consumption_per_week_Light, alcohol_consumption_per_week_Moderate, alcohol_consumption_per_week_Heavy]]
+
+    return input_data
 
 @router.get("/diagnosis", status_code=status.HTTP_200_OK)
 async def diabetes_diagnosis(hba1c: float, glucose_fasting: int):
-    model = joblib.load("backend/models/cat_1.joblib")
-    input_data = prepare_for_diagnosis(hba1c, glucose_fasting)
+    model = joblib.load("backend/models/model1.pkl")
+    input_data = [[glucose_fasting, hba1c]]
     prediction = model.predict_proba(input_data)
     result = True if prediction[0][1] >= thresh1 else False
+
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(pd.DataFrame(input_data, columns=chosen_columns_1))
+    shap_df = pd.DataFrame(shap_values, columns=chosen_columns_1)
+
     if result:
-        return {"message": "You probably have diabetes."}
+        return {"message": f"shap values: {shap_df.to_dict()}", "diagnosis": "You probably have diabetes."}
     else:
-        return {"message": "You probably do not have diabetes."}
+        return {"message": f"shap values: {shap_df.to_dict()}", "diagnosis": "You probably don't have diabetes."}
     
 @router.get("/risk_score", status_code=status.HTTP_200_OK)
-async def diabetes_risk_score(family_history: int, bmi: float, age: int, waist_to_hip_ratio: float, physical_activity: int):
-    model = joblib.load("backend/models/cat_2.joblib")
-    input_data = prepare_for_diagnosis_2(family_history, bmi, age, waist_to_hip_ratio, physical_activity)
-    result = model.predict_proba(input_data)
-    print(result)
-    return {"risk_score": f"{result}"}
+async def diabetes_risk_score(family_history: int, bmi: float, age: int, waist_to_hip_ratio: float, physical_activity: int, alcohol_consumption_per_week: int):
+    model = joblib.load("backend/models/model2.pkl")
+    input_data = prepare_for_diagnosis(family_history, bmi, age, waist_to_hip_ratio, physical_activity, alcohol_consumption_per_week)
+    prediction = model.predict_proba(input_data)
+    result = True if prediction[0][1] >= thresh2 else False
+    
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(pd.DataFrame(input_data, columns=chosen_columns_2))
+    shap_df = pd.DataFrame(shap_values, columns=chosen_columns_2)
+
+    if result:
+        return {"message": f"shap values: {shap_df.to_dict()}", "diagnosis": "Your risk of diabetes is high."}
+    else:
+        return {"message": f"shap values: {shap_df.to_dict()}", "diagnosis": "Your risk of diabetes is low."}
